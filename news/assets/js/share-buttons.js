0474d55d7d2385e0e23817ae4b816ee930bf3f46
danbrincat <dan.brincat07@gmail.com>
2023-02-23 17:13:36 +0000

    remove share text

--- news/assets/js/share-buttons.js
+++ news/assets/js/share-buttons.js
@@ -1,5 +1,4 @@
 document.write('\
-	<p>Share:</p>\
 	<a onclick="window.open("https://www.facebook.com/sharer/sharer.php?u="+pageUrl")"><span class="fab fa-facebook-f social-bttn" style="color:#2274b9"></span></a>\
 	<a onclick="window.open("https://twitter.com/intent/tweet?text="+pageHeader+pageUrl+pageBanner)"><span class="fab fa-twitter social-bttn" style="color:#4d9ed8"></span></a>\
 	<a onclick="window.open("http://pinterest.com/pin/create/button/?url="+pageUrl+"&media="+pageBanner+"&description="")"><span class="fab fa-pinterest-p social-bttn" style="color:#e60019"></span></a>\
