bc8b545968824bef7816e2dea1d264ff102d4165
danbrincat <dan.brincat07@gmail.com>
2023-02-23 16:45:55 +0000

    Update share-buttons.js

--- news/assets/js/share-buttons.js
+++ news/assets/js/share-buttons.js
@@ -1,1 +1,8 @@
-document.write('&lt;p>Share: &lt;&#8725;p> &lt;a onclick="window.open(&#39;https:&#8725;&#8725;www.facebook.com&#8725;sharer&#8725;sharer.php?u=&#39;+pageUrl">&lt;span class="fab fa-facebook-f social-bttn" style="color:#2274b9">&lt;&#8725;span>&lt;&#8725;a> &lt;a onclick="window.open(&#39;https:&#8725;&#8725;twitter.com&#8725;intent&#8725;tweet?text=&#39;+pageHeader+pageUrl+pageBanner)">&lt;span class="fab fa-twitter social-bttn" style="color:#4d9ed8">&lt;&#8725;span>&lt;&#8725;a> &lt;a onclick="window.open(&#39;http:&#8725;&#8725;pinterest.com&#8725;pin&#8725;create&#8725;button&#8725;?url=&#39;+pageUrl+&#39;&media=&#39;+pageBanner+&#39;&description=&#39;)"> &lt;span class="fab fa-pinterest-p social-bttn" style="color:#e60019">&lt;&#8725;span>&lt;&#8725;a> &lt;a onclick="window.open(&#39;mailto:?subject=&#39;+pageHeader+&#39;&body=&#39;+pageUrl)">&lt;span class="far fa-envelope social-bttn" style="color:#000">&lt;&#8725;span>&lt;&#8725;a> &lt;a onclick="copyLink()">&lt;span class="fas fa-link social-bttn" style="color:#0f65ef">&lt;&#8725;span>&lt;&#8725;a>');
+document.write("\
+	<p>Share:</p>\
+	<a onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+pageUrl"><span class="fab fa-facebook-f social-bttn" style="color:#2274b9"></span></a>\
+	<a onclick="window.open('https://twitter.com/intent/tweet?text='+pageHeader+pageUrl+pageBanner)"><span class="fab fa-twitter social-bttn" style="color:#4d9ed8"></span></a>\
+	<a onclick="window.open('http://pinterest.com/pin/create/button/?url='+pageUrl+'&media='+pageBanner+'&description=')"><span class="fab fa-pinterest-p social-bttn" style="color:#e60019"></span></a>\
+	<a onclick="window.open('mailto:?subject='+pageHeader+'&body='+pageUrl)"><span class="far fa-envelope social-bttn" style="color:#000"></span></a>\
+	<a onclick="copyLink()"><span class="fas fa-link social-bttn" style="color:#0f65ef"></span></a>\
+");
