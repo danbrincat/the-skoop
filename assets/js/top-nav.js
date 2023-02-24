17b77a52afc25c47e313f259a6930329cea4916c
danbrincat <dan.brincat07@gmail.com>
2023-02-24 14:40:12 +0000

    Update top-nav.js

--- assets/js/top-nav.js
+++ assets/js/top-nav.js
@@ -11,23 +11,12 @@ document.write('\
 				<ul>\
 					<li class="current_page_item"><a href="/">Home</a></li>\
 					<li>\
-						<a href="#">Topics</a>\
+						<a href="/news">News</a>\
 						<ul>\
-							<li><a href="#">Lorem ipsum dolor</a></li>\
-							<li><a href="#">Magna phasellus</a></li>\
-							<li>\
-								<a href="#">Phasellus consequat</a>\
-								<ul>\
-									<li><a href="#">Lorem ipsum dolor</a></li>\
-									<li><a href="#">Phasellus consequat</a></li>\
-									<li><a href="#">Magna phasellus</a></li>\
-									<li><a href="#">Etiam dolore nisl</a></li>\
-								</ul>\
-							</li>\
-							<li><a href="#">Veroeros feugiat</a></li>\
+							<li><a href="/news">All Articles</a></li>\
 						</ul>\
 					</li>\
-					<li><a href="left-sidebar.html">Quizzes</a></li>\
+					<li><a href="/quizzes">Quizzes</a></li>\
 					<script async src="https://cse.google.com/cse.js?cx=012922c0c43404565"></script>\
 					<div id="searchBox">\
 						<div class="gcse-search"></div>\
